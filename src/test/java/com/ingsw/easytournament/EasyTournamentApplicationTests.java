b1ac0597aaa4c10100a396f7b4aa56d5d93f7a8b
crxstqn <lprcst04h30c588y@studenti.unical.it>
2025-11-07 14:55:51 +0100

    creazione progetto

---
+++ src/test/java/com/ingsw/easytournament/EasyTournamentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingsw.easytournament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyTournamentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
